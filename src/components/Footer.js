d02a02c4f99374b9e65c7c53708561477784f910
Ryota <ryoenoki2013@gmail.com>
2022-01-24 04:47:14 +0900

    hover effects added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer>
       <ul>
-        <li><a href="https://twitter.com/" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-twitter-square"></i></a></li>
+        <li><a href="https://twitter.com/" rel="noreferrer" target="_blank" className="footer-sns"><span><i className="fab fa-twitter-square"></i></span></a></li>
         <li><a href="https://www.facebook.com/" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-facebook-square"></i></a></li>
         <li><a href="https://www.instagram.com/" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-instagram-square"></i></a></li>
         <li><a href="https://github.com/ERy03" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-github-square"></i></a></li>
