833b360559da1a1feb5a2e2614183f1490c3d2f6
Ryota <ryoenoki2013@gmail.com>
2022-01-24 02:03:31 +0900

    info buttons

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,12 +3,11 @@ import React from "react"
 function ContactButtons(){
   return (
     <div className="info-button-container">
-      <button><i class="fas fa-envelope"></i>Email</button>
-      <button><i class="fab fa-linkedin"></i>LinkedIn</button>
+      <a href="mailto:ryoenoki21@gmail.com"><button className="email-button"><span className="button-icon"><i class="fas fa-envelope" /></span>Email</button></a>
+      <a href="https://linkedin.com/in/ryota-enoki" rel="noreferrer" target="_blank"><button className="linkedin-button"><span className="button-icon"><i class="fab fa-linkedin" /></span>LinkedIn</button></a>
     </div>
   )
 }
-
 
 export default function Info() {
   return (
