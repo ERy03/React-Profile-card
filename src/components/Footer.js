c2cffd602ddd98e947bccfa677dc02a547a7327f
Ryota <ryoenoki2013@gmail.com>
2022-01-24 03:45:31 +0900

    image added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer>
       <ul>
-        <li><a href="https://twitter.com/" rel="noreferrer" target="_blank" className="footer-sns"><i class="fab fa-twitter-square"></i></a></li>
-        <li><a href="https://www.facebook.com/" rel="noreferrer" target="_blank" className="footer-sns"><i class="fab fa-facebook-square"></i></a></li>
-        <li><a href="https://www.instagram.com/" rel="noreferrer" target="_blank" className="footer-sns"><i class="fab fa-instagram-square"></i></a></li>
-        <li><a href="https://github.com/ERy03" rel="noreferrer" target="_blank" className="footer-sns"><i class="fab fa-github-square"></i></a></li>
+        <li><a href="https://twitter.com/" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-twitter-square"></i></a></li>
+        <li><a href="https://www.facebook.com/" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-facebook-square"></i></a></li>
+        <li><a href="https://www.instagram.com/" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-instagram-square"></i></a></li>
+        <li><a href="https://github.com/ERy03" rel="noreferrer" target="_blank" className="footer-sns"><i className="fab fa-github-square"></i></a></li>
       </ul>
     </footer>
   )
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,10 +1,11 @@
 import React from "react"
+import portrait from "../images/portrait.JPG"
 
 function ContactButtons(){
   return (
     <div className="info-button-container">
-      <a href="mailto:ryoenoki21@gmail.com"><button className="email-button"><span className="button-icon"><i class="fas fa-envelope" /></span>Email</button></a>
-      <a href="https://linkedin.com/in/ryota-enoki" rel="noreferrer" target="_blank"><button className="linkedin-button"><span className="button-icon"><i class="fab fa-linkedin" /></span>LinkedIn</button></a>
+      <a href="mailto:ryoenoki21@gmail.com"><button className="email-button"><span className="button-icon"><i className="fas fa-envelope" /></span>Email</button></a>
+      <a href="https://linkedin.com/in/ryota-enoki" rel="noreferrer" target="_blank"><button className="linkedin-button"><span className="button-icon"><i className="fab fa-linkedin" /></span>LinkedIn</button></a>
     </div>
   )
 }
@@ -12,8 +13,8 @@ function ContactButtons(){
 export default function Info() {
   return (
   <div className="info">
-    {/* <img src="http://source.unsplash.com/random/317x317/?portrait" className="portrait-img" alt="portrait" /> */}
-    <h1>John / Jane Doe</h1>
+    <img src={portrait} className="portrait-img" alt="portrait" />
+    <h1>Ryota Enoki</h1>
     <p>Frontend Developer</p>
     <a href="https://ryotaenoki.me/" rel="noreferrer" target="_blank">ryotaenoki.me</a>
     < ContactButtons />
